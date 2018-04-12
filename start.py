f05ca1cac7c88a988a513d61f4a930ad81595da9
Ziyad Edher <ziyad.edher@gmail.com>
2018-04-12 10:56:35 -0400

    Begin project structure with boilerplate code

---
+++ start.py
@@ -0,0 +1,10 @@
+"""Begin the standalone game.
+
+This module begins execution of the game as a standalone program. It should never be imported,
+only executed directly to begin the standalone program.
+"""
+
+
+if __name__ == "__main__":
+    # TODO: run program
+    pass
